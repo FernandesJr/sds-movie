bb3fa81515bc9a2ab241bfab0bfe85e2cfe3ec62
Fernandes Júnior <fernaandes.jr@gmail.com>
2022-01-12 11:19:12 -0300

    Security config - start

---
+++ src/main/java/com/fernandesDeveloper/sdsmovie/SdsMovieApplication.java
@@ -0,0 +1,13 @@
+package com.fernandesDeveloper.sdsmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdsMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdsMovieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernandesDeveloper/sdsmovie/SdsMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandesDeveloper.sdsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdsMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
